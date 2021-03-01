14c94607c72bec2461eb452046a7201c4d6068b7
Ikaraz <m.djuka@hotmail.com>
2021-03-01 22:39:36 +0100

    changed token

--- src/main/java/com/example/Bet365Odds/bet365Services/UpcomingMatches.java
+++ src/main/java/com/example/Bet365Odds/bet365Services/UpcomingMatches.java
@@ -25,7 +25,7 @@ public class UpcomingMatches implements Runnable{
         while(true){
             try {
                 //TODO: promeni token kada uzmes novi
-                conn = (HttpURLConnection) new URL("https://api.b365api.com/v1/bet365/inplay_filter?sport_id=1&token=73510-dKGX4Zgh2ekp0C").openConnection();
+                conn = (HttpURLConnection) new URL("https://api.b365api.com/v1/bet365/inplay_filter?sport_id=1&token=74978-Ac8xiivPuhouM0").openConnection();
                 conn.setConnectTimeout(5000);
                 conn.setRequestProperty("Content-Type", "application/json: charset=UTF-8");
                 conn.setDoInput(true);
